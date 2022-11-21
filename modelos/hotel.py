18729b55ab38faee1dffdc3ceac4f14ca9d93d42
carloslocatellijr <call.jmail@gmail.com>
2022-11-20 22:09:49 -0300

    atualiza hotel

--- modelos/hotel.py
+++ modelos/hotel.py
@@ -1,5 +1,5 @@
 from modelos.mongo_table import MongoTable
-import hospede
+from hospede import Hospede as hospede
 
 class Hotel(MongoTable):
     def __init__(self, nome: str, cidade: str, num_quartos: int):
@@ -9,15 +9,13 @@ class Hotel(MongoTable):
         self.quartos = [Quarto(n, None) for n in range(0, self.num_quartos)]
 
     def reserva(self, hospede: object, quarto: object):
-        if not None in self.quartos:
+        if not None in (n[1] for n in self.quartos):
             return 'Hotel está com lotação máxima'
         else:
             for q in self.quartos:
                 if q[1] is None:
                     q[1] = hospede
                     break
-                
-
 
     def check_in(self, hospede: str):
         pass
@@ -25,8 +23,13 @@ class Hotel(MongoTable):
     def check_out(self, quarto: object):
         pass
 
+    def __repr__(self) -> str:
+        return f'Hotel ({self.nome}, em {self.cidade})'
+
 class Quarto():
     def __init__(self, numero_quarto: int, tipo: str = 'básico', ocupado: bool = False):
         self.quarto = numero_quarto
         self.tipo = tipo
 
+    def __repr__(self) -> str:
+        return f'Quarto ({self.quarto})'
--- tests/test_hotel.py
+++ tests/test_hotel.py
@@ -1,8 +1,12 @@
-import modelos.hotel as hotel
+import unittest
+from hotel import Hotel
 
 
-h = hotel.Hotel('Hotel California', 'San Francisco', 6)
+h = Hotel('Hotel California', 'San Francisco', 6)
 
 def  test_hotel_nome():
     assert h.nome == 'Hotel California',  'Erro no nome'
 
+
+if __name__ == '__main__':
+    unittest.main()
